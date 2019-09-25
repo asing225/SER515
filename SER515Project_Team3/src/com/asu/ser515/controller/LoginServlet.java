3776cd6d232652dabfdb01f088a158725c98880b
asing225 <amanjot5552@gmail.com>
2019-09-25 15:01:25 -0700

    Added sample login page and admin page to be loaded after login.

--- SER515Project_Team3/src/com/asu/ser515/controller/LoginServlet.java
+++ SER515Project_Team3/src/com/asu/ser515/controller/LoginServlet.java
@@ -1,5 +1,12 @@
 package com.asu.ser515.controller;
 
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author amanjotsingh
  * date 09/25/2019
@@ -8,6 +15,24 @@ package com.asu.ser515.controller;
  * user and redirect to their respective web pages. 
  * */
 
-public class LoginServlet {
-
+@SuppressWarnings("serial")
+public class LoginServlet extends HttpServlet{
+	
+	private String adminPage = "admin.html";
+	
+	public void init(ServletConfig config) throws ServletException { 
+    	super.init(config);
+   	}
+	
+	// doPost method to handle form submit coming from web page
+	public void doPost(HttpServletRequest req, HttpServletResponse res) {
+        String firstname = req.getParameter("firstName");
+        try {
+			req.getRequestDispatcher(adminPage).forward(req, res);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ServletException e) {
+			e.printStackTrace();
+		}
+	}
 }
