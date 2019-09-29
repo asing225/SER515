fb2fdbdce7b8361c4f6a992087b181d227584199
asing225 <amanjot5552@gmail.com>
2019-09-29 00:47:34 -0700

    Added Model class for the teacher.

---
+++ SER515Project_Team3/src/com/asu/ser515/model/Teacher.java
@@ -0,0 +1,42 @@
+package com.asu.ser515.model;
+
+/**
+ * Model class for the Teacher. This class has all the properties associated with the 
+ * teacher. 
+ * 
+ * @author amanjotsingh
+ * @date 09/25/2019
+ * 
+ * */
+
+public class Teacher {
+
+	private String firstName;
+	private String lastName;
+	
+	// default constructor
+	public Teacher() { }
+	
+	// class constructors
+	public Teacher(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	// getters and setters
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
