8f88f674f30032fd4a2b50467ea679a9527276c5
asing225 <amanjot5552@gmail.com>
2019-10-06 22:29:19 -0700

    Teacher Quiz creation branch with formatted code.

--- SER515Project_Team3/src/com/asu/ser515/controller/LoginServlet.java
+++ SER515Project_Team3/src/com/asu/ser515/controller/LoginServlet.java
@@ -12,8 +12,8 @@ import com.asu.ser515.services.helper.DBConnServiceHelper;
 import com.asu.ser515.services.impl.DBConnServiceImpl;
 
 /**
- * Controller class to handle the login functionality of the application. It will authenticate the
- * user and redirect to their respective web pages. 
+ * Controller class to handle the login functionality of the application. It
+ * will authenticate the user and redirect to their respective web pages.
  * 
  * @author amanjotsingh
  * @date 09/25/2019
@@ -21,88 +21,87 @@ import com.asu.ser515.services.impl.DBConnServiceImpl;
  * @author kushagrjolly
  * @date 09/29/2019
  * 
- * */
+ */
 
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
-	
+
 	private String errorPage = "error.html";
 	private String exceptionPage = "exception.html";
 	private String adminPage = "admin.html";
 	private String teacherPage = "teacher.html";
 	private String studentGrade_1Page = "student1.html";
 	private String studentGrade_6Page = "student2.html";
-	
-	public void init(ServletConfig config) throws ServletException { 
-    	super.init(config);
-   	}
-	
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+
 	// doPost method to handle form submit coming from web page
 	public void doPost(HttpServletRequest req, HttpServletResponse res) {
-        String password = req.getParameter("password");
-        String userName = req.getParameter("username");
-		User oldUser = new User(userName,password);
-        DBConnServiceImpl serviceImpl = new DBConnServiceImpl();
-        int dbResult = serviceImpl.authenticateUser(oldUser);
-        if(dbResult == 0) {
-        	//User not present
-        	try {
+		String password = req.getParameter("password");
+		String userName = req.getParameter("username");
+		User oldUser = new User(userName, password);
+		DBConnServiceImpl serviceImpl = new DBConnServiceImpl();
+		int dbResult = serviceImpl.authenticateUser(oldUser);
+		if (dbResult == 0) {
+			// User not present
+			try {
 				req.getRequestDispatcher(errorPage).forward(req, res);
 			} catch (IOException ioExc) {
 				ioExc.printStackTrace();
 			} catch (ServletException servletExc) {
 				servletExc.printStackTrace();
 			}
-        }
-        if(dbResult == -1) {
-        	//Exception in database
-        	try {
+		}
+		if (dbResult == -1) {
+			// Exception in database
+			try {
 				req.getRequestDispatcher(exceptionPage).forward(req, res);
 			} catch (IOException ioExc) {
 				ioExc.printStackTrace();
 			} catch (ServletException servletExc) {
 				servletExc.printStackTrace();
 			}
-        }
-        else {
-        	DBConnServiceHelper dbHelper = new DBConnServiceHelper();
-    		String role=dbHelper.mapDBtoUsertype(dbResult);
-    		if (role.equalsIgnoreCase("admin")){
-    			try {
-    				req.getRequestDispatcher(adminPage).forward(req, res);
-    			} catch (IOException ioExc) {
-    				ioExc.printStackTrace();
-    			} catch (ServletException servletExc) {
-    				servletExc.printStackTrace();
-    			}
-    		}
-    		if (role.equalsIgnoreCase("teacher")){
-    			try {
-    				req.getRequestDispatcher(teacherPage).forward(req, res);
-    			} catch (IOException ioExc) {
-    				ioExc.printStackTrace();
-    			} catch (ServletException servletExc) {
-    				servletExc.printStackTrace();
-    			}
-    		}
-    		if (role.equalsIgnoreCase("studentGrade_1")){
-    			try {
-    				req.getRequestDispatcher(studentGrade_1Page).forward(req, res);
-    			} catch (IOException ioExc) {
-    				ioExc.printStackTrace();
-    			} catch (ServletException servletExc) {
-    				servletExc.printStackTrace();
-    			}
-    		}
-    		if (role.equalsIgnoreCase("studentGrade_6")){
-    			try {
-    				req.getRequestDispatcher(studentGrade_6Page).forward(req, res);
-    			} catch (IOException ioExc) {
-    				ioExc.printStackTrace();
-    			} catch (ServletException servletExc) {
-    				servletExc.printStackTrace();
-    			}
-    		}
-        }        
+		} else {
+			DBConnServiceHelper dbHelper = new DBConnServiceHelper();
+			String role = dbHelper.mapDBtoUsertype(dbResult);
+			if (role.equalsIgnoreCase("admin")) {
+				try {
+					req.getRequestDispatcher(adminPage).forward(req, res);
+				} catch (IOException ioExc) {
+					ioExc.printStackTrace();
+				} catch (ServletException servletExc) {
+					servletExc.printStackTrace();
+				}
+			}
+			if (role.equalsIgnoreCase("teacher")) {
+				try {
+					req.getRequestDispatcher(teacherPage).forward(req, res);
+				} catch (IOException ioExc) {
+					ioExc.printStackTrace();
+				} catch (ServletException servletExc) {
+					servletExc.printStackTrace();
+				}
+			}
+			if (role.equalsIgnoreCase("studentGrade_1")) {
+				try {
+					req.getRequestDispatcher(studentGrade_1Page).forward(req, res);
+				} catch (IOException ioExc) {
+					ioExc.printStackTrace();
+				} catch (ServletException servletExc) {
+					servletExc.printStackTrace();
+				}
+			}
+			if (role.equalsIgnoreCase("studentGrade_6")) {
+				try {
+					req.getRequestDispatcher(studentGrade_6Page).forward(req, res);
+				} catch (IOException ioExc) {
+					ioExc.printStackTrace();
+				} catch (ServletException servletExc) {
+					servletExc.printStackTrace();
+				}
+			}
+		}
 	}
 }
--- SER515Project_Team3/src/com/asu/ser515/model/Teacher.java
+++ SER515Project_Team3/src/com/asu/ser515/model/Teacher.java
@@ -1,22 +1,23 @@
 package com.asu.ser515.model;
 
 /**
- * Model class for the Teacher. This class has all the properties associated with the 
- * teacher. 
+ * Model class for the Teacher. This class has all the properties associated
+ * with the teacher.
  * 
  * @author amanjotsingh
  * @date 09/25/2019
  * 
- * */
+ */
 
 public class Teacher {
 
 	private String firstName;
 	private String lastName;
-	
+
 	// default constructor
-	public Teacher() { }
-	
+	public Teacher() {
+	}
+
 	// class constructors
 	public Teacher(String firstName, String lastName) {
 		this.firstName = firstName;
