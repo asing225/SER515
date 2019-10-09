e3e0a1617cee79883eb279f89746bb9e8d945347
asing225 <amanjot5552@gmail.com>
2019-10-08 20:19:21 -0700

    Added header for the application and downloaded library files

---
+++ SER515Project_Team3/WebContent/js/header.js
@@ -0,0 +1,10 @@
+/**
+ * This file contains the javascript code for the application header
+ * 
+ * @author - amanjotsingh
+ * @date - 10/08/2019
+ * 
+ */
+$(function() {
+	$("#header").load("header.html");
+});
