eb2d9f4e0f9e77c1231ed90777dc1597885a6ddf
mahendrarrao <mahendra.raok@gmail.com>
2019-10-16 18:56:37 -0700

    output as alert of mathematical expression built using blocks

--- SER515Project_Team3/WebContent/js/student1.js
+++ SER515Project_Team3/WebContent/js/student1.js
@@ -3,3 +3,24 @@
   var demoWorkspace = Blockly.inject('blocklyDiv',
       {media: './lib/blockly-master/media/',
        toolbox: document.getElementById('toolbox')});
+
+       Blockly.Xml.domToWorkspace(document.getElementById('startBlocks'),
+                                  demoWorkspace);
+
+
+       function runCode() {
+         // Generate JavaScript code and run it.
+
+         var answer;
+         window.LoopTrap = 1000;
+         Blockly.JavaScript.INFINITE_LOOP_TRAP =
+             'if (--window.LoopTrap == 0) throw "Infinite loop.";\n';
+         var code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
+         Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+         try {
+           eval(code);
+         } catch (e) {
+           alert(e);
+           //document.getElementById("textbox3").value = answer;
+         }
+       }
