30f206e198260e3fb11414930aaba7800980b1e0
mahendrarrao <mahendra.raok@gmail.com>
2019-10-19 20:05:30 -0700

    Comments with notice addedstudent1.js

--- SER515Project_Team3/WebContent/js/student1.js
+++ SER515Project_Team3/WebContent/js/student1.js
@@ -1,4 +1,28 @@
 //JS file to inject the toolbox element
+
+/*
+*@title : JS file to inject the toolbox element
+*@Authors : Mahendra Rao, Kushagr Jolly, Anurag Mishra, Amanjot Singh, Akshay Kmar Dileep
+*@Project - SER515 Team 3 "DragOn"
+
+* @license
+* Visual Blocks Editor
+*
+* Copyright 2018 Google Inc.
+* https://developers.google.com/blockly/
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use any of these files except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*   http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 'use strict';
   var demoWorkspace = Blockly.inject('blocklyDiv',
       {media: './lib/blockly-master/media/',
@@ -18,9 +42,10 @@
          var code = Blockly.JavaScript.workspaceToCode(demoWorkspace);
          Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
          try {
-           eval(code);
+           answer = eval(code);
          } catch (e) {
            alert(e);
+           //print answer in HTML element -> textbox3
            //document.getElementById("textbox3").value = answer;
          }
        }
