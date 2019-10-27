d13bb2119a300c6c350d3d9074c8c36af1ac1c93
Akshay Kumar Dileep <akshaykumardileep@Akshays-MacBook-Pro.local>
2019-10-26 18:08:29 -0700

    <#68 teacherQuizCreation>

--- SER515Project_Team3/WebContent/js/index1.js
+++ SER515Project_Team3/WebContent/js/index1.js
@@ -14,7 +14,20 @@ count = 1
     function AddText() {
       if(count<=10){
         count = count + 1
-        $('<div id="heading"><h3>Question' +count+'</h3></div><input type="text" placeholder="Question'+count+'" name="Question'+count+'"></br><textarea placeholder="Solution" name="Solution'+count+'" type="text"></textarea><hr></div></div>').appendTo('#wrapper1');
+        $('<div id= "head'+count+'"><div id="heading'+count+'"><h3>Question' +count+'</h3></div><input type="text" placeholder="Question'+count+'" name="Question'+count+'"></br><textarea placeholder="Solution" name="Solution'+count+'" type="text"></textarea><hr></div></div></div>').appendTo('#wrapper1');
+    }
+  }
+    $('#btRemove').click(function () {
+      RemoveText();
+});
+    function RemoveText() {
+      var val = "head"
+      var value = val.concat(count);
+      console.log(value);
+      if(count>1){
+      var element = document.getElementById(value);
+      document.getElementById("wrapper1").removeChild(element)
+      count = count - 1;
     }
   }
 });
