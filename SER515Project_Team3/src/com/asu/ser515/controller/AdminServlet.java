7dc86ef4bfb373ab5c1973a2528c352e501e2eba
Anurag Mishra <anurag933103@gmail.com>
2019-11-19 10:47:11 -0700

    #184:setting up admin servlet

---
+++ SER515Project_Team3/src/com/asu/ser515/controller/AdminServlet.java
@@ -0,0 +1,23 @@
+package com.asu.ser515.controller;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * @author anurag933103
+ * 19/11/2019
+ */
+@SuppressWarnings("serial")
+public class AdminServlet extends HttpServlet {
+	
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+	
+	public void doPut(HttpServletRequest req, HttpServletResponse res) {
+		
+	}
+
+}
